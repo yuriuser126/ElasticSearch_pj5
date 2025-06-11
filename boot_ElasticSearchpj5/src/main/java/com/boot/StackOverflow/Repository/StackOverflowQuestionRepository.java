2164f4932850c401b1055665f6f115d1f57d0f31
sonbg2025 <syupeu@gmail.com>
2025-06-11 17:16:09 +0900

    no message

--- boot_ElasticSearchpj5/src/main/java/com/boot/StackOverflow/Repository/StackOverflowQuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.boot.StackOverflow.Repository;
-
-
-import com.boot.StackOverflow.DTO.StackOverflowQuestion;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StackOverflowQuestionRepository extends MongoRepository<StackOverflowQuestion, String> {
-}
